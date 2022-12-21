6af2d33af5c76b9b5ff4699387f95e1cafc67720
George Saint Hilaire <georgesainthilaire@Georges-iMac.local>
2022-12-21 18:56:40 -0500

    First commit

---
+++ src/Data.js
@@ -0,0 +1,44 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Mistletoe Kiss (Light)",
+    price: "Free",
+    desc: "Gin, rosemary syrup, lemon juice, soda water, cranberry juice",
+    img: "images/IMG_1659.jpeg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Mister Grinch (Light)",
+    price: "Free",
+    desc: "Gin, mint, jalapeño, grapefruit juice, lime juice, agave, ginger beer.",
+    img: "images/IMG_1653.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Christmas Mai Tai (Strong)",
+    price: "Free",
+    desc: "Spiced rum, white rum, lime juice, cranberry juice, orange curaçao, orgeat syrup, cinnamon syrup",
+    img: "images/IMG_1655.jpeg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Under the Mistletoe (Strong)",
+    price: "Free",
+    desc: "Mezcal, amaretto, pomegranate juice, lemon juice, simple syrup",
+    img: "images/IMG_1657.jpeg",
+  },
+  {
+    id: "chefhcbcv",
+    name: "Peppermint Mocha Espresso Martini (Sweet)",
+    price: "Free",
+    desc: "Vodka, cold brew coffee, coffee liquor, mocha syrup, peppermint extract.",
+    img: "images/IMG_1654.jpeg",
+  },
+  {
+    id: "awkfhcbcv",
+    name: "Coquito (Sweet)",
+    price: "Free",
+    desc: "Coconut milk, condensed milk, evaporated milk, dark rum, cinnamon, nutmeg.",
+    img: "images/IMG_1658.jpeg",
+  }
+];
